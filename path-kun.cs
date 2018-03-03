272c4267254937cad976fded391f28b81d69f3e5
WAKABAYASHI Hiroaki <wak@users.noreply.github.com>
2018-03-03 09:55:44 +0900

    Refactoring

--- path-kun.cs
+++ path-kun.cs
@@ -34,46 +34,49 @@ class SharablePath
 {
     DriveMap driveMap;
     string originalPath;
+    string sharablePath;
     
     public SharablePath(DriveMap map, string originalPath)
     {
         this.driveMap = map;
         this.originalPath = originalPath;
+        this.sharablePath = this.originalPath;
+        
+        extractPathRecursively();
     }
 
-    private string convertToSharablePath()
+    private void extractPathRecursively()
     {
-        int maxRepeat = 26;
-        string currentPath = this.originalPath;
-
-        Console.WriteLine(currentPath);
-        while (maxRepeat-- > 0) {
-            MatchCollection mc = Regex.Matches(currentPath, @"^([a-zA-Z]:)\\?(.*)$");
-
-            if (mc.Count == 0)
+        Console.WriteLine(this.sharablePath);
+        for (int maxRepeat = 26; maxRepeat > 0; maxRepeat--) {
+            if (!extractPath())
                 break;
+            
+            Console.WriteLine(" -> " + this.sharablePath);
+        }
+    }
+
+    private bool extractPath()
+    {
+        MatchCollection mc = Regex.Matches(this.sharablePath, @"^([a-zA-Z]:)\\?(.*)$");
         
+        if (mc.Count != 0) {
             string driveName = mc[0].Groups[1].Value;
             string restPath = mc[0].Groups[2].Value;
-            if (!this.driveMap.hasNetworkDrive(driveName))
-                return currentPath;
-
-            string networkPath = this.driveMap.getPath(driveName);
-            string nextPath = System.IO.Path.Combine(networkPath, restPath);
-
-            Console.WriteLine(" -> " + nextPath);
             
-            if (currentPath == nextPath)
-                break;
+            if (this.driveMap.hasNetworkDrive(driveName)) {
+                string networkPath = this.driveMap.getPath(driveName);
+                this.sharablePath = System.IO.Path.Combine(networkPath, restPath);
 
-            currentPath = nextPath;
+                return true;
+            }
         }
-        return currentPath;
+        return false;
     }
 
     override public string ToString()
     {
-        return convertToSharablePath();
+        return this.sharablePath;
     }
 }
 
@@ -100,15 +103,15 @@ class MountedPath
 
 class DriveMap
 {
-    Dictionary<string, MountedPath> drivePathMap;
+    Dictionary<string, MountedPath> deviceMap;
     
     public DriveMap()
     {
-        this.drivePathMap = new Dictionary<string, MountedPath>();
-        this.loadMountedPathMap();
+        this.deviceMap = new Dictionary<string, MountedPath>();
+        this.loadDeviceMap();
     }
 
-    void loadMountedPathMap()
+    private void loadDeviceMap()
     {
         using (ManagementObjectSearcher searcher = new ManagementObjectSearcher())
         {
@@ -124,45 +127,46 @@ class DriveMap
 
                         string driveName = (mo["Name"] ?? string.Empty).ToString();
                         string path = (mo["ProviderName"] ?? string.Empty).ToString();
-                        if (path.Length == 0) {
-                            // subst command case
-                            
-                            StringBuilder pathInformation = new StringBuilder(250);
-                            QueryDosDevice(driveName, pathInformation, 250);
-                            
-                            // Strip the \??\ prefix.
-                            path = pathInformation.ToString().Remove(0, 4);
-                        }
-                        this.drivePathMap.Add(driveName.ToUpper(), new MountedPath(driveName, path));
+                        if (path.Length == 0)
+                            path = loadDosDrivePath(driveName);
+                        this.deviceMap.Add(driveName.ToUpper(), new MountedPath(driveName, path));
                     }
                 }
             }
         }
     }
     
+    // subst command case
+    private string loadDosDrivePath(string driveName)
+    {                    
+        StringBuilder pathInformation = new StringBuilder(250);
+        QueryDosDevice(driveName, pathInformation, 250);
+                            
+        // Strip the \??\ prefix.
+        return pathInformation.ToString().Remove(0, 4);
+    }
+    
     public bool hasNetworkDrive(string driveName)
     {
         driveName = driveName.ToUpper();
-        
-        if (!this.drivePathMap.ContainsKey(driveName)) {
+        if (!this.deviceMap.ContainsKey(driveName))
             return false;
-        }
 
-        return this.drivePathMap[driveName].isNetworkPath();
+        return this.deviceMap[driveName].isNetworkPath();
     }
 
     public string getPath(string driveName)
     {
-        return this.drivePathMap[driveName.ToUpper()].path;
+        return this.deviceMap[driveName.ToUpper()].path;
     }
 
     public void dump()
     {
-        foreach (KeyValuePair<string, MountedPath> pair in this.drivePathMap) {   
+        foreach (KeyValuePair<string, MountedPath> pair in this.deviceMap) {   
             Console.WriteLine(string.Format("{0}={1}", pair.Key, pair.Value.path));
         }
     }
     
     [DllImport("kernel32.dll")]
-    static extern uint QueryDosDevice(string lpDeviceName, StringBuilder lpTargetPath, int ucchMax);
+    private static extern uint QueryDosDevice(string lpDeviceName, StringBuilder lpTargetPath, int ucchMax);
 }
