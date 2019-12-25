6514d1039255e94a9c4b9cf72e94c18031ef27e9
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2019-12-25 20:06:11 +0900

    support 32bit Windows OS

--- build_with_csc.bat
+++ build_with_csc.bat
@@ -1,5 +1,26 @@
+@echo off
+
 set CSC=
 set OPT=
+
+REM 32bit
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe
+    set OPT=
+)
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe
+    set OPT=
+)
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+    set OPT=
+)
+
+REM 64bit
 
 if EXIST C:\Windows\Microsoft.NET\Framework64\v2.0.50727\csc.exe (
     set CSC=C:\Windows\Microsoft.NET\Framework64\v2.0.50727\csc.exe
@@ -16,8 +37,13 @@ if EXIST C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe (
     set OPT=
 )
 
-%CSC% %OPT% /target:winexe /out:path-kun-basic.exe PathKunBasic\PathKunBasic.cs PathKunLib\PathKunLib.cs
-%CSC% %OPT% /target:winexe /out:path-kun-pathonly.exe PathKunPathOnly\PathKunPathOnly.cs PathKunLib\PathKunLib.cs
-%CSC% %OPT% /target:winexe /out:path-kun-explorer.exe PathKunExplorer\PathKunExplorer.cs PathKunLib\PathKunLib.cs
+echo ============================================================
+echo = CSC: %CSC%
+echo = OPT: %OPT%
+echo ============================================================
+
+%CSC% %OPT% /nologo /target:winexe /out:PathKunBasic.exe PathKunBasic\PathKunBasic.cs PathKunLib\PathKunLib.cs
+%CSC% %OPT% /nologo /target:winexe /out:PathKunPathOnly.exe PathKunPathOnly\PathKunPathOnly.cs PathKunLib\PathKunLib.cs
+%CSC% %OPT% /nologo /target:winexe /out:PathKunExplorer.exe PathKunExplorer\PathKunExplorer.cs PathKunLib\PathKunLib.cs
 
 pause
--- build_with_msbuild.bat
+++ build_with_msbuild.bat
@@ -3,6 +3,25 @@
 set CSC=
 set OPT=
 
+REM 32bit
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v2.0.50727\msbuild.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
+    set OPT=
+)
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v3.5\msbuild.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v3.5\msbuild.exe
+    set OPT=
+)
+
+if EXIST C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+    set OPT=
+)
+
+REM 64bit
+
 if EXIST C:\Windows\Microsoft.NET\Framework64\v2.0.50727\msbuild.exe (
     set CSC=C:\Windows\Microsoft.NET\Framework64\v2.0.50727\msbuild.exe
     set OPT=
@@ -18,16 +37,20 @@ if EXIST C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe (
     set OPT=
 )
 
-%CSC% %OPT% /p:Configuration=Release path-kun.sln
+%CSC% %OPT% /nologo /p:Configuration=Release path-kun.sln
 
 echo =================================
-echo ビルドが完了しました。
-echo 以下のファイルをご利用ください。
-echo.
-echo   bin\Release\
-echo     PathKunBasic.exe
-echo     PathKunExplorer.exe
-echo     PathKunPathOnly.exe
-echo.
+if %ERRORLEVEL% == 0 (
+    echo ビルドが完了しました。
+    echo 以下のファイルをご利用ください。
+    echo.
+    echo   bin\Release\
+    echo     PathKunBasic.exe
+    echo     PathKunExplorer.exe
+    echo     PathKunPathOnly.exe
+) else (
+    echo ビルドに失敗しました。
+)
 
+echo.
 pause
